72b273a282b4d9a4787c312f7008d212b33d9cf6
Nasir <nasirwaheed999@gmail.com>
2024-03-15 16:34:54 +0500

    completed expnadation page

--- src/components/Common/BusinessExpandationCard.jsx
+++ src/components/Common/BusinessExpandationCard.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 
-function BusinessExpandationCard({ logo, title, prices, increments }) {
+function BusinessExpandationCard({ logo, title, prices, increments, key }) {
   return (
-    <div className="bg-white p-[30px_10px_40px_10px]">
-      <div className="">
-        <h1>{logo}</h1>
-        <span>{title}</span>
-        <h1>{prices}</h1>
-        <span>{increments}</span>
+    <div
+      key={key}
+      className="flex flex-col gap-5 bg-white p-[59px_10px_40px_20px] rounded-lg w-full h-72"
+    >
+      <h1>{logo}</h1>
+      <span className="text-DarkGray font-semibold w-2/3">{title}</span>
+      <div className="flex flex-col gap-2">
+        <h1 className="text-Blue text-3xl font-bold">{prices}</h1>
+        <span className="text-Green font-semibold">{increments}</span>
       </div>
     </div>
   );
--- src/components/Home/CompanyExpandation.jsx
+++ src/components/Home/CompanyExpandation.jsx
@@ -1,16 +1,56 @@
 import React from "react";
+import BusinessExpandationCard from "../Common/BusinessExpandationCard";
 
 function CompanyExpandation() {
   const cardsData = [
     {
       icon: "he",
-      title: "gdfgdfgd",
-      prices: "fdgdfbdf",
+      title: "Nominal GDP 2023",
+      prices: "64.1 Billions",
+    },
+    {
+      icon: "he",
+      title: "Nominal GFCF 2023",
+      prices: "SAR 279 Billions",
+    },
+    {
+      icon: "he",
+      title: "FDI Inward Stock, 2023",
+      prices: "SAR 1,017 Billions",
+      increments: "10.2% % increase",
+    },
+    {
+      icon: "he",
+      title: "FDI Inflows",
+      prices: "SAR 8.1 Billions",
+      increments: "0.098 % increase",
+    },
+    {
+      icon: "he",
+      title: "Investment License, 2023",
+      prices: "SAR 1,819 Billions",
+      increments: "93.3% % increase",
+    },
+    {
+      icon: "he",
+      title: "Transport & Logistics Investement Expexted by 2023",
+      prices: "64.1 Billions",
+      increments: "93.3% % increase",
+    },
+    {
+      icon: "he",
+      title: "Nominal GDP 2023",
+      prices: "64.1 Billions",
+    },
+    {
+      icon: "he",
+      title: "Nominal GDP 2023",
+      prices: "64.1 Billions",
     },
   ];
   return (
-    <div className="bg-LightSky">
-      <div className="flex flex-col justify-center items-center p-[70px_100px_70px_100px] gap-2">
+    <div className="bg-LightSky p-[70px_100px_70px_100px]">
+      <div className="flex flex-col justify-center items-center  gap-2">
         <span className="text-Sky">KINGDOM OF SAUDI ARABIA</span>
         <h1 className="text-Blue font-bold text-4xl">Why Expand to KSA?</h1>
         <p className="text-center w-[40%] text-DarkGray">
@@ -18,11 +58,15 @@ function CompanyExpandation() {
           commonly used to demonstrate the visual form
         </p>
       </div>
-      <div className="bg-white p-[30px_10px_40px_10px]">
-        {cardsData.map(({ icon, title, prices }, index) => (
-          <div className="">
-            
-          </div>
+      <div className="grid grid-cols-3 pt-10 gap-5">
+        {cardsData.map(({ icon, title, prices, increments }, index) => (
+          <BusinessExpandationCard
+            key={index}
+            logo={icon}
+            title={title}
+            prices={prices}
+            increments={increments}
+          />
         ))}
       </div>
     </div>
